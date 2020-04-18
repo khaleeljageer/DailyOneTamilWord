751ab6093b8a7fe2f0cd678eeff9e9639ede954a
Khaleel Jageer <jskcse4@gmail.com>
2020-04-19 04:35:05 +0530

    local settings update

--- local_settings.py
+++ local_settings.py
@@ -2,8 +2,8 @@ from os import environ
 
 # Configuration for Twitter API
 ENABLE_TWITTER_POSTING = True
-MY_CONSUMER_KEY = 'PJTrFbZYxKl3U20M7LVQVxzwo'
-MY_CONSUMER_SECRET = 'kIi8hXHOf1FllAO3UPSoiUmidDseq3TbWBJagnBcFR91rgGuOl'
-MY_ACCESS_TOKEN_KEY = '162929468-hGz3dfvJixYf11BsYGr80akRExWwcLKcpkTXjaoM'
-MY_ACCESS_TOKEN_SECRET = 'FQa36b8jljojCHVqQom10klwezm3NCGL4fYBU0eXDhlgT'
+MY_CONSUMER_KEY = 'your key'
+MY_CONSUMER_SECRET = 'your secret key'
+MY_ACCESS_TOKEN_KEY = 'your access token'
+MY_ACCESS_TOKEN_SECRET = 'your access secret'
 DEBUG = True  # Set this to False to start Tweeting live
