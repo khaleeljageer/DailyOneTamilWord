abf86c78d0dba201572e6ac3200ccb098bf2a5b9
Khaleel Jageer <jskcse4@gmail.com>
2020-04-19 04:36:14 +0530

    debug status

--- local_settings.py
+++ local_settings.py
@@ -6,4 +6,4 @@ MY_CONSUMER_KEY = 'your key'
 MY_CONSUMER_SECRET = 'your secret key'
 MY_ACCESS_TOKEN_KEY = 'your access token'
 MY_ACCESS_TOKEN_SECRET = 'your access secret'
-DEBUG = True  # Set this to False to start Tweeting live
+DEBUG = False  # Set this to False to start Tweeting live
