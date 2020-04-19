41d6237724a567f6759117d1dcfddcbfa001c9b7
Khaleel Jageer <jskcse4@gmail.com>
2020-04-19 13:18:59 +0530

    debug mode enabled

--- local_settings.py
+++ local_settings.py
@@ -10,5 +10,5 @@ MY_ACCESS_TOKEN_SECRET = 'FQa36b8jljojCHVqQom10klwezm3NCGL4fYBU0eXDhlgT'
 # Configuration for Parse API
 X_PARSE_APPLICATION_ID = 'uegNg5eWDj8UY19XkhzeQMUWcvkNqza17TaLRq22'
 X_PARSE_REST_API_KEY = 'qyIGngnRAqRvf8oXf9r2MhhaN1iDzcr68V5M91oo'
-DEBUG = False  # Set this to False to start Tweeting live
+DEBUG = True  # Set this to False to start Tweeting live
 ODDS = 8
