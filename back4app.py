a3b59be5048bd08abc4ce0cf075143d61c539760
Khaleel Jageer <jskcse4@gmail.com>
2020-04-19 11:49:23 +0530

    keys removed

--- back4app.py
+++ back4app.py
@@ -1,13 +1,14 @@
 import requests
 import json
+from local_settings import *
 
 
 def update_status(objectId):
     url = "https://parseapi.back4app.com/classes/WordCorpus/"+objectId
     payload = {'status': True}
     header = {
-        'X-Parse-Application-Id': 'uegNg5eWDj8UY19XkhzeQMUWcvkNqza17TaLRq22',
-        'X-Parse-REST-API-Key': 'qyIGngnRAqRvf8oXf9r2MhhaN1iDzcr68V5M91oo',
+        'X-Parse-Application-Id': X_PARSE_APPLICATION_ID,
+        'X-Parse-REST-API-Key': X_PARSE_REST_API_KEY,
         'Content-Type': 'application/json'
     }
 
@@ -19,9 +20,9 @@ def update_status(objectId):
 class Back4App():
     def get_sentance(self):
         header = {
-            'content-type': 'application/json',
-            'X-Parse-Application-Id': 'uegNg5eWDj8UY19XkhzeQMUWcvkNqza17TaLRq22',
-            'X-Parse-REST-API-Key': 'qyIGngnRAqRvf8oXf9r2MhhaN1iDzcr68V5M91oo'
+            'Content-Type': 'application/json',
+            'X-Parse-Application-Id': X_PARSE_APPLICATION_ID,
+            'X-Parse-REST-API-Key': X_PARSE_REST_API_KEY,
         }
         url = "https://parseapi.back4app.com/classes/WordCorpus?where=%7B%22status%22%3Afalse%7D"
         data = requests.get(url, headers=header)
@@ -31,7 +32,7 @@ class Back4App():
         sentence = ('சொல் : %s,\nபொருள் : %s' %
                     (results['word'], results['meaning']))
         update_status(results['objectId'])
-        tags = '#தினம் #ஒரு #தமிழ் #சொல்'
+        tags = '\n#தினம் #ஒரு #தமிழ் #சொல்'
         return sentence+tags
 
 
