91a1a95d9c6e800faeda1c77770e7e3694439873
Khaleel Jageer <jskcse4@gmail.com>
2020-04-19 19:06:36 +0530

    revert

--- back4app.py
+++ back4app.py
@@ -31,7 +31,7 @@ class Back4App():
         sentence = ('சொல் : %s,\nபொருள் : %s' %
                     (results['word'], results['meaning']))
         update_status(results['objectId'])
-        tags = '\n#தினம் #ஒரு #தமிழ் #சொல்'
+        tags = '\n#தினமொரு #தமிழ்_சொல்'
         return sentence+tags
 
 
